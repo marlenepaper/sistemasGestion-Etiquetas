b1927f5b8a47bf7712da3058bfd25e92f4569340
marlenepaper <marlenepaper@hotmail.com>
2024-11-07 17:45:18 +0100

    codigo inicial

--- Main.java
+++ Main.java
@@ -1,21 +1,10 @@
-public class Main{
-	public static void main (String[] args){
-		if(args.length == 0){
-			System.out.println("No hay argumentos");	
-		}else{
-			System.out.println("Hay " + args.length + " argumentos");
-		}
-
-		try{
-
-			for(int i = 0; i < args.length; i++){
-				System.out.println(args[i]);	
-			}
-
-		}catch(IndexOutOfBoundsException e){
-
-			System.out.println("Error: " + e);
-
-		}
-	}
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No hay argumentos");
+        } else {
+            System.out.println("Hay " + args.length + " argumentos");
+        }
+    }
 }
+
